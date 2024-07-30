587a3bff80516f2a00bfabb04bfb75f2f9ecccb8
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-07-30 18:01:53 -0400

    quiz

--- quiz_game.py
+++ quiz_game.py
@@ -8,3 +8,21 @@ def ask_question(question, correct_answer):
         return False
     
 
+def quiz_game():
+    score = 0
+
+    print("Welcome to the Quiz Game.")
+    print("Let's get started\n")
+
+    if ask_question("What is the capital of France?", "Paris"):
+        score+=1
+
+    if ask_question("What is 5 + 7?", "12"):
+        score+=1
+    if ask_question("Who wrote 'Hamlet?'", "Sharspeare"):
+        score+=1
+    if ask_question("What is the chemical symbol for water?", "H2O"):
+        score+=1
+    print("\n You Got:", score, "out of 4 questions correct!")
+
+quiz_game()
