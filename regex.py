a9a96cd5cc734a70a9a3ac49228e12b2eb4a97dc
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-07 14:01:30 -0400

    User input Pattern stage 2

--- regex.py
+++ regex.py
@@ -1,7 +1,15 @@
-email = input("What's your email? ").strip()
+""" email = input("What's your email? ").strip()
 
 if "@" in email and "." in email:
     print("Valid")
 
 else:
-    print("Invalid")
+    print("Invalid") """
+
+email = input("What's your email?").strip()
+
+username, domain = email.split("@")
+if (username) and ("." in domain):
+    print(username)
+else:
+    print("Invalid")    
