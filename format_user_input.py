e3d1a80a1049411ff73233361f0c4eb13b798100
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-09 14:38:19 -0400

    Formate User Input

---
+++ format_user_input.py
@@ -0,0 +1,28 @@
+""" name = input("What's your name?")
+if "," in name:
+    last, first = name.split(",")
+    name = f"{first} {last}"
+
+print(f"Hello {name}") """
+
+""" import re
+
+name = input("What's your nsme?").strip()
+
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+   
+    name = matches.group(2) + " " + matches.group(1)
+   
+print(f"Hello {name}") """
+
+import re
+
+name = input("What's your nsme?").strip()
+
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+
+    last, first = matches.group(2) 
+    name = f"{first} {last}"   
+print(f"Hello {name}")
