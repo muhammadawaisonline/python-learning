70fce399002fc38f73b6b082b876e929c8e9b59d
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-17 15:06:10 -0400

    More Precise

--- regex.py
+++ regex.py
@@ -1,10 +1,20 @@
      #Getting user input and trying to validate it.
-email = input("What's your email? ").strip()
+""" email = input("What's your email? ").strip()
 
 if "@" in email and "." in email:
     print("Valid")
 
 else:
-    print("Invalid")
+    print("Invalid") """
    
-   
+   #making more precise our condition
+
+email = input("What's your email?").strip()
+
+username, domain = email.split("@")
+if (username) and ("." in domain):
+    print(username)
+else:
+    print("Invalid")    
+
+          
